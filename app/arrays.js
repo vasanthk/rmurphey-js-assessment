42e771f1de6cd63965180044f825231f692ce574
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-02 14:41:14 -0800

    Array add/remove items

--- app/arrays.js
+++ app/arrays.js
@@ -43,23 +43,27 @@ exports.arraysAnswers = {
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert : function(arr, item, index) {
