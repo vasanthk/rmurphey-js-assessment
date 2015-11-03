7c6f2dcdaf9d466e7cbda56f5458625e350a7846
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-02 23:15:47 -0800

    Functions call apply etc

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,66 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray : function(fn, arr) {
-
+  argsAsArray: function (fn, arr) {
+    // 'this' is null here.
+    return fn.apply(null, arr);
   },
 
-  speak : function(fn, obj) {
-
+  speak: function (fn, obj) {
+    return fn.call(obj);
   },
 
-  functionFunction : function(str) {
-
+  functionFunction: function (str) {
+    return function (arg) {
+      return str + ', ' + arg;
+    }
   },
 
-  makeClosures : function(arr, fn) {
+  makeClosures: function (arr, fn) {
+    var ret = [];
 
+    var makeFn = function (val) {
+      return function () {
+        return fn(val);
+      };
+    };
+
+    for (var i = 0; i < arr.length; i++) {
+      ret.push(makeFn(arr[i]));
+    }
   },
 
-  partial : function(fn, str1, str2) {
-
+  partial: function (fn, str1, str2) {
+    return function (str3) {
+      return fn.call(null, str1, str2, str3);
+    }
   },
 
-  useArguments : function() {
+  useArguments: function () {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
 
+    return sum;
   },
 
-  callIt : function(fn) {
-
+  callIt: function (fn) {
+    // Arguments are passed in along with the fn as well hence, splice it.
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    fn.apply(null, args);
   },
 
-  partialUsingArguments : function(fn) {
-
+  partialUsingArguments: function (fn) {
+    // Arguments are passed in along with the fn as well hence, splice it.
+    var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+    return function () {
+      var moreArgs = args.concat(Array.prototype.slice.call(arguments));
+      return fn.apply(null, moreArgs);
+    };
   },
 
-  curryIt : function(fn) {
+  curryIt: function (fn) {
 
   }
 };
