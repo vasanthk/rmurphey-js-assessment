06da7b428e2d480c19ca0d4cd9d7cbb4321025dd
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-02 17:51:03 -0800

    Async with jQuery promises

--- app/async.js
+++ app/async.js
@@ -1,11 +1,24 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
-
+  async: function (value) {
+    var deferred = $.Deferred();
+    setTimeout(function () {
+      deferred.resolve(value);
+    }, 10);
+    return deferred.promise();
   },
 
-  manipulateRemoteData : function(url) {
+  manipulateRemoteData: function (url) {
+    var deferred = $.Deferred();
 
+    $.ajax(url).then(function (resp) {
+      var people = resp.people.map(function (person) {
+        return person.name;
+      });
+      deferred.resolve(people.sort());
+    });
+
+    return deferred.promise();
   }
 };
