fed34b60de64e880b232323da37d4c0f98af5a73
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-02 21:33:11 -0800

    count recursion

--- app/count.js
+++ app/count.js
@@ -1,7 +1,25 @@
 exports = (typeof window === 'undefined') ? global : window;
 
-exports.countAnswers =  {
-  count : function (start, end) {
+exports.countAnswers = {
+  count: function (start, end) {
+    var timeout;
 
+    // Recurse through run()
+    // Note: setInterval could be used too, but it will continue to call the fn and needs to be cleared once start = end.
+    function run() {
+      console.log(start++);
+
+      if (start <= end) {
+        timeout = setTimeout(run, 100);
+      }
+    }
+
+    run();
+
+    return {
+      cancel: function () {
+        timeout && clearTimeout(timeout);
+      }
+    }
   }
 };
