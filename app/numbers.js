7506c1b2e2969fa2141c72979c6a55d7b683e6ac
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-06 15:13:27 -0800

    Numbers :(

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    // TODO: Someone explain what this is. I don't get it :(
+    return 1 & (num >> (bit -1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    // Convert an eight-bit number to a binary string
+    // No idea :(
 
   },
 
   multiply: function(a, b) {
-
+    // No idea :(
   }
 };
