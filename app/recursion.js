844528dae208423ce8202e81e4a17c127c61c7c0
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-08 22:04:10 -0800

    Recursion

--- app/recursion.js
+++ app/recursion.js
@@ -2,19 +2,44 @@ exports = (typeof window === 'undefined') ? global : window;
 
 // TODO: Take this one: https://www.codecademy.com/courses/javascript-lesson-205/0/1
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function (data, dirName) {
+    var listOfFiles = [];
+    var dirs = [];
+
+    processDir(data);
+
+    function processDir(dir) {
+      var files = dir.files;
+
+      dirs.push(dir.dir);
+
+      for (var i = 0; i < files.length; i++) {
+        file = files[i];
+        if (typeof file === 'string') {
+          if (!dirName || dirs.indexOf(dirName) > -1) {
+            listOfFiles.push(files[i]);
+          }
+        } else {
+          processDir(files[i]);
+        }
+      }
+
+      // One you reach the end of the current directory tree - Pop it!
+      dirs.pop();
+    }
+
+    return listOfFiles;
+  },
+
+  permute: function (arr) {
 
   },
 
-  permute: function(arr) {
+  fibonacci: function (n) {
 
   },
 
-  fibonacci: function(n) {
-
-  },
-
-  validParentheses: function(n) {
+  validParentheses: function (n) {
 
   }
 };
