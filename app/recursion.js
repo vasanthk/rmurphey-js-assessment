95da9485884c38b84e8b8940c5560c3f1c16f4e1
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-09 23:27:19 -0800

    Fix fibonacci + validParanthesis soln

--- app/recursion.js
+++ app/recursion.js
@@ -77,18 +77,30 @@ exports.recursionAnswers = {
   // TODO: CHekc if this can be simplified.
   fibonacci: function (n) {
     // Naive rerursive approach
-    var fib = function(n) {
-      var result = 0;
-      if (n <= 2) {
-        return n - 1;
-      }
-      result = fib(n - 1) + fib(n - 2);
-      return result;
-    };
-    return fib(n);
+    function fib(n) {
+      return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    }
+    return fib(n)
   },
 
   validParentheses: function (n) {
+    function nPair(n) {
+      if (n == 0)
+        return [""];
 
+      var result = [];
+      for (var i = 0; i < n; ++i) {
+        var lefts = nPair(i);
+        var rights = nPair(n - i - 1);
+
+        for (var l = 0; l < lefts.length; ++l)
+          for (var r = 0; r < rights.length; ++r)
+            result.push("(" + lefts[l] + ")" + rights[r]);
+      }
+
+      return result;
+    }
+
+    return nPair(n);
   }
 };
