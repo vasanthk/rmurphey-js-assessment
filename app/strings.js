c074d17bdbdcf0c2836636bc5a72a309eaca7067
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-11-10 00:31:14 -0800

    Word wrap

--- app/strings.js
+++ app/strings.js
@@ -5,11 +5,42 @@ exports.stringsAnswers = {
 
   },
   wordWrap: function (str, cols) {
+    var strArr = str.split('');
+    var i = cols;
 
+    function checkMore() {
+      for (i; i < strArr.length; i++) {
+        if (strArr[i] === ' ') {
+          strArr[i] = '\n';
+          i += cols;
+          return;
+        }
+      }
+    }
+
+    function checkLess() {
+      for (i; i > 0; i--) {
+        if (strArr[i] === ' ') {
+          strArr[i] = '\n';
+          i += cols;
+          return;
+        }
+      }
+      checkMore();
+    }
+
+    while (i < strArr.length) {
+      if (strArr.indexOf(' ') < 0) {
+        return strArr.join('')
+      }
+      checkLess();
+    }
+
+    return strArr.join('')
   },
   reverseString: function (str) {
     var result = '';
-    for (var i = str.length-1; i >= 0; i--) {
+    for (var i = str.length - 1; i >= 0; i--) {
       result += str[i];
     }
     return result;
